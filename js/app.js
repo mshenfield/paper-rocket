37d626d556478abe4006f63b69aabeecc17a9967
Max Shenfield <shenfieldmax@gmail.com>
2015-10-05 08:45:21 -0500

    Added some basic baddies with collision handling
    You can now "eat" alien bad  guys with your rocket ship and you'll slowly fade into a ghost ship.

--- js/app.js
+++ js/app.js
@@ -7,15 +7,20 @@ var cursors;
 var player;
 var spacebar;
 var background;
+var enemies;
 
 function preload(){
   game.load.image('rocket', 'assets/rocket.png');
+  game.load.image('alien', 'assets/alien.png');
   game.load.image('paper', 'assets/paper.png');
 }
 
 function create(){
   background = game.add.tileSprite(0, 0, GAMEWIDTH, GAMEHEIGHT, 'paper');
+
   player = game.add.sprite(0, 0, 'rocket');
+
+  enemies = game.add.group();
 
   game.physics.startSystem(Phaser.Physics.ARCADE);
 
@@ -24,10 +29,27 @@ function create(){
   cursors = game.input.keyboard.createCursorKeys();
 
   spacebar = game.input.keyboard.addKey(Phaser.Keyboard.Spacebar);
+
+  game.time.events.loop(Phaser.Timer.SECOND * 2, spawnEnemy, this);
+}
+
+function spawnEnemy(){
+  var enemy = enemies.create(game.world.width, game.world.randomY, 'alien');
+  game.physics.arcade.enable(enemy);
+  enemy.outOfBoundsKill = true;
+  enemy.body.velocity.x = -200;
+}
+
+function playerVsEnemy(player, enemy){
+  console.log(enemy);
+  enemy.kill();
+  player.alpha -= .1;
 }
 
 function update(){
-  background.tilePosition.x -= 2;
+  game.physics.arcade.overlap(player, enemies, playerVsEnemy, null, this);
+
+  background.tilePosition.x -= player.body.velocity.x / game.time.desiredFps;
 
   player.body.velocity.x = 0;
   player.body.velocity.y = 0;
